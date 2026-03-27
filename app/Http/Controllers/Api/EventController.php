be7dcf5a63970c54a78f726d0b9507b9db53c45a
henriquediasjr <henriquediasjr@gmail.com>
2026-03-26 23:04:53 -0300

    feat: making store and update controllers

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -12,7 +13,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        return Event::all();
     }
 
     /**
@@ -20,23 +21,41 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        echo $request;
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time',
+            ]),
+            'user_id' => 1
+        ]);
+
+        return $event;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        //
+        return $event;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        return $event->update([
+              $request->validate([
+                  'name' => 'sometimes|string|max:255',
+                  'description' => 'nullable|string',
+                  'start_time' => 'sometimes|date',
+                  'end_time' => 'sometimes|date|after:start_time',
+              ])
+        ]);
     }
 
     /**
